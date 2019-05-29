7dd0c64178685a6126e8b50b531e8f88205b5793
Eric Crosson <eric.s.crosson@utexas.edu>
2019-05-29 14:38:52 -0500

    Use explicit sorting function in example

--- examples/simple.ts
+++ examples/simple.ts
@@ -2,10 +2,10 @@ import { inplaceRemoveDuplicatesFromSorted } from '../src/remove-duplicates-from
 import randomInt from 'random-int'
 
 const list = Array(10).fill(100).map(randomInt)
-const listWithDuplicates = [...list, ...list].sort()
+const listWithDuplicates = [...list, ...list].sort((x: number, y: number) => x - y)
 
 console.log(listWithDuplicates)
-//=>[ 14, 14, 17, 17, 30, 30, 4, 4, 41, 41, 69, 69, 89, 89, 90, 90, 93, 93, 97, 97 ]
+//=>[ 23, 23, 45, 45, 63, 63, 66, 66, 74, 74, 78, 78, 85, 85, 90, 90, 92, 92, 99, 99 ]
 
 console.log(inplaceRemoveDuplicatesFromSorted(listWithDuplicates))
-//=>[ 14, 17, 30, 4, 41, 69, 89, 90, 93, 97 ]
+//=>[ 24, 45, 63, 66, 74, 78, 85, 90, 92, 99 ]
